2b00d7974222b9430b205f53f55afb57a09be785
Charles Dansereau <charles19982009@hotmail.com>
2023-03-25 11:30:02 -0400

    modified policy

--- policies/customPolicy.py
+++ policies/customPolicy.py
@@ -19,7 +19,7 @@ class CustomPolicy(FeedForwardPolicy):
 class CustomLSTMPolicy(LstmPolicy):
     def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm=256, reuse=False, **_kwargs):
         super().__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm, reuse,
-                         net_arch=[256, 'lstm', dict(vf=[128, 128, 128], pi=[128, 128, 128])],
+                         net_arch=[128, 64, 'lstm', dict(vf=[128, 128, 128], pi=[128, 128])],
                          layer_norm=True, feature_extraction="mlp", **_kwargs)
 
 ## Create and wrap the environment
