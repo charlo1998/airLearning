66d964dd81ba967f5dcc190beb538dddd6863081
Charles Dansereau <charles19982009@hotmail.com>
2023-05-11 20:20:14 -0400

    test setup

--- settings_folder/settings.py
+++ settings_folder/settings.py
@@ -185,8 +185,8 @@ this means the "percieved" latency doesn't increase linearly with the clockspeed
 timedActions = False
 positionActions = True
 action_discretization = 16 #this needs to be a square number and greater than one if timedActions is set to true! 
-number_of_sensors = 12
-k_sensors = 3  #the maximum amount of sensors the agent can choose at any time
+number_of_sensors = 24
+k_sensors = 24  #the maximum amount of sensors the agent can choose at any time
 assert(action_discretization > 1)
 if timedActions:
     assert(int(math.sqrt(action_discretization) + 0.5) ** 2 == action_discretization)
@@ -239,8 +239,8 @@ runs_to_do = 1
 i_run =  1#this needs to be the same value as runs_to_do
 assert(runs_to_do == i_run)
 buffer_size = 50000  #replay buffer: this affects critically the iteration speed as the buffer gets filled (for dqn airsim)
-use_checkpoint = True
-training_steps_cap = 400000
+use_checkpoint = False
+training_steps_cap = 50000
 nb_steps_warmup = 5000 #iterations are really fast during this phase
 curriculum_learning = True
 verbose = True
