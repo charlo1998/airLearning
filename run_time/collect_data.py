ac7a3639044aca65f49a2e3395595dcabd862e1f
Charles Dansereau <charles19982009@hotmail.com>
2023-05-18 19:26:34 -0400

    Revert "Merge branch 'complete-pointcloud' into bug-algo"
    This reverts commit 6932dc9704cff79044a02bee73e1f23b0de315b6.

--- run_time/collect_data.py
+++ run_time/collect_data.py
@@ -105,7 +105,7 @@ def main():
 
 
 
-    algo = "GOFAI"
+    algo = "A2C-B"
     task_type = "test"
     msgs.mode = task_type
     model_weights_list_to_test = [os.path.expanduser("~") + "/workspace/airlearning/airlearning-rl/data/" + algo + "/model.pkl"] # change A2C for baselines
